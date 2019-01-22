9caa18d2b520355b46fd17c0a640cc392033656b
ShrawanKumar Sharma <shrawan25.sharma@gmail.com>
2019-01-23 00:32:41 +0530

    Project init

---
+++ Book_N_Play_Replicate_Android/app/src/main/java/book_n_play_replicate/com/book_n_play_replicate/MainActivity.kt
@@ -0,0 +1,13 @@
+package book_n_play_replicate.com.book_n_play_replicate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
